02ce6825cec0fcbb82b8b190d783141aa0dee14e
ka52k <76809740+ka52k@users.noreply.github.com>
2021-01-04 18:28:16 +0900

    20210104-2変更

--- src/index.js
+++ src/index.js
@@ -12,8 +12,29 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // ボタン（完了）タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 完了ボタンが押されたら、その親タグ（div）を未完了リストから削除する。
+    const deleteTarget = completeButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // ボタン（削除）タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 削除ボタンが押されたら、その親タグ（div）を未完了リストから削除する。
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
